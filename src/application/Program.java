29328fc939a1516be48850df257fb614c1806e02
cassiocoltri <cassiocoltri@gmail.com>
2022-10-25 17:58:35 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,8 +14,13 @@ public class Program {
 		Department obj = new Department(1, "Ablubleu");
 		
 		Seller seller = new Seller(21, "Borsalino", "borsalino@gmail.com", new Date(), 1000.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		
 		System.out.println(obj);
 		System.out.println(seller);
+		System.out.println(sellerDao);
 
 	}
 
