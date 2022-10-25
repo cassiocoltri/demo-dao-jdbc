f6d280963b17952c3bc43677f968f7c75108ff5c
cassiocoltri <cassiocoltri@gmail.com>
2022-10-25 17:30:48 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Ablubleu");
+		
+		Seller seller = new Seller(21, "Borsalino", "borsalino@gmail.com", new Date(), 1000.0, obj);
 		System.out.println(obj);
+		System.out.println(seller);
 
 	}
 
