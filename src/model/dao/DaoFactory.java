3a880eb2aa311308f966908674b51620319278e7
cassiocoltri <cassiocoltri@gmail.com>
2022-11-04 17:14:09 -0300

    DepartmentDao implementation and test

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,12 +1,17 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoIJDBC;
 
 public class DaoFactory {
 	
 	public static SellerDao createSellerDao() {
 		return new SellerDaoIJDBC(DB.getConnection());
+	}
+	
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
 	}
 
 }
